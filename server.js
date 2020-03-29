eed13b06aa018cdba1b3ab7abc8507d7b11386a8
jnlrzn <62641905+jnlrzn@users.noreply.github.com>
2020-03-29 16:08:41 +0800

    Update server.js

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const express = require('express')
 const app = express()
 const auth = require('http-auth')
 
-const port = process.env.PORT || 29802
+const port = process.env.PORT || 3001
 
 // Basic auth
 // Set USERNAME and PASSWORD environment variables
